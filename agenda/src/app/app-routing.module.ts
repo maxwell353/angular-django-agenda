f068ede968bec7c377f2bf29c6fd8fb130e682bc
unknown <maxwell3531@gmail.com>
2021-08-25 09:27:36 -0300

    Desenvolvido um guarda de controle de acesso(se não tiver acesso volta para tela de login).

--- agenda/src/app/app-routing.module.ts
+++ agenda/src/app/app-routing.module.ts
@@ -4,9 +4,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './autenticador/login/login.component';
 import { HomeComponent } from './paginas/home/home.component';
 
+import { GuardaGuard } from './util/guarda.guard';
+
 const routes: Routes = [
   {path: '', component: LoginComponent},
-  {path: 'home', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'home', component: HomeComponent, canActivate: [GuardaGuard]},
+  {path: '**', redirectTo: '/home' }
 ];
 
 @NgModule({
