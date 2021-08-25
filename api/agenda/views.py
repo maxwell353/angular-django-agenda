a0881dd39405babbf9c1a9c2b9f6ed64e155f0fc
unknown <maxwell3531@gmail.com>
2021-08-25 08:55:13 -0300

    Desenvolvido segurança dos dados com authentication(Token) do django rest, efetuado um migrate para aplicar o authtoken, criado rota de login para gerar token valido.

--- api/agenda/views.py
+++ api/agenda/views.py
@@ -3,6 +3,24 @@ from agenda.models import Contato
 from rest_framework import viewsets, authentication
 from agenda.serializers import UserSerializer, GroupSerializer, ContatoSerializer
 
+from django.contrib.auth import authenticate
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.authtoken.models import Token
+from django.http import JsonResponse
+import json
+
+@csrf_exempt
+def login(request):
+    body_unicode = request.body.decode('utf-8')
+    body = json.loads(body_unicode)
+    if body['username']:
+        user = authenticate(username=body['username'], password=body['password'])
+        if user:
+            token = Token.objects.get_or_create(user=user)
+            return JsonResponse( {'token': token[0].key, 'id': user.id, 'usuario': user.username, 'email': user.email, 'nome': user.first_name} )
+        else:
+            return JsonResponse( {'error': "Error"} )
+
 class UserViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
--- api/api/urls.py
+++ api/api/urls.py
@@ -28,4 +28,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^', include(router.urls)),
+    url(r'login/', views.login),
 ]
